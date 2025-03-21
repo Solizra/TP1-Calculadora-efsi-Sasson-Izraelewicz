1e5651a8ec1cf4413e91ab4c7b280502f1a30380
INDIANA SASSON <48043270@ort>
2025-03-21 10:40:58 -0300

    avances

--- codigo.js
+++ codigo.js
@@ -3,17 +3,25 @@ let operaciones = [];
 let numeros = [];
 
 let agregar = (n) =>{ 
-    if(calculo.innerText === "0"){ //hayq ue poner que esto se cumpmpla si es un numero
+    if(calculo.innerText === "0"){ 
         calculo.innerText = n;
+        numeros.push(n);
     }
     else if (n == 1 || n==2 || n==3 || n == 4 || n==5 || n==6 || n==7 || n==8 || n==9 || n==0) {
         calculo.innerText += n;
+        numeros.push(n);
     } 
-        
+        //cada vez que se ponga una operacion que el numero ingresado se junte y forme un numero
     if (n == "+" || n =="-" || n=="x" || n=="/") {
         calculo.innerText += ` ${n} `
-        operaciones.pop(n);
+        operaciones.push(n);
+        numeros.push("*");
+    
     }
+
+    //ver si funciona
+    console.log(numeros)
+    console.log(operaciones)
 }
 
 let eliminar = () =>{
@@ -21,5 +29,7 @@ let eliminar = () =>{
 }
 
 let Calcular = () =>{
+    console.log(numeros.indexOf("*")) //VER COMO HACER PARA QUE CADA VEZ QUE ENCUENTRA UN *, JUNTE TODAS LAS POSICIONES ANTERIORES (LAS CONCATENE) PARA DESPUES PASARLO A NUMERO Y CALCULAR
+
 
 }
